ee6ae889b33b8eba8c867ef9e5e246b1781845ea
akira wakaba <wakabadou@gmail.com>
2019-10-14 23:04:00 +0900

    #7 読み込みモード時にConvertEncodingFilterとConvertLienFeedFilterを併用しCSV入力を行うと失敗する
    - 改行文字検索打ち切りタイミングの変更
    - メソッド名のtypoを修正

--- tests/traits/StreamFilterTestTrait.php
+++ tests/traits/StreamFilterTestTrait.php
@@ -30,13 +30,13 @@ trait StreamFilterTestTrait
      *
      * @param   string  $expected       予想される値
      * @param   string  $value          実行する値
-     * @param   array   $steram_wrapper ストリームコンテキスト
+     * @param   array   $stream_wrapper ストリームコンテキスト
      */
-    public function assertWriteStreamFilterSame(string $expected, string $value, array $steram_wrapper) : void
+    protected function assertWriteStreamFilterSame(string $expected, string $value, array $stream_wrapper) : void
     {
-        $steram_wrapper['resource']   = 'php://temp';
+        $stream_wrapper['resource']   = 'php://temp';
 
-        $write_stream   = $this->convertSteramWrapper($steram_wrapper);
+        $write_stream   = $this->convertStreamWrapper($stream_wrapper);
 
         $fp     = @\fopen($write_stream, 'ab');
         @\fwrite($fp, $value);
@@ -58,13 +58,13 @@ trait StreamFilterTestTrait
      *
      * @param   string  $expected       予想される値
      * @param   string  $value          実行する値
-     * @param   array   $steram_wrapper ストリームコンテキスト
+     * @param   array   $stream_wrapper ストリームコンテキスト
      */
-    protected function assertWriteStreamFilterNotSame(string $expected, string $value, array $steram_wrapper) : void
+    protected function assertWriteStreamFilterNotSame(string $expected, string $value, array $stream_wrapper) : void
     {
-        $steram_wrapper['resource']   = 'php://temp';
+        $stream_wrapper['resource']   = 'php://temp';
 
-        $write_stream   = $this->convertSteramWrapper($steram_wrapper);
+        $write_stream   = $this->convertStreamWrapper($stream_wrapper);
 
         $fp     = @\fopen($write_stream, 'ab');
         @\fwrite($fp, $value);
@@ -87,13 +87,13 @@ trait StreamFilterTestTrait
      * @param   array   $expected           予想される値
      * @param   string  $csv_text           実行する値
      * @param   int     $stream_chunk_size  ストリームラッパーのチャンクサイズ
-     * @param   array   $steram_wrapper     ストリームコンテキスト
+     * @param   array   $stream_wrapper     ストリームコンテキスト
      */
-    protected function assertCsvInputStreamFilterSame(array $expected, string $csv_text, int $stream_chunk_size, array $steram_wrapper) : void
+    protected function assertCsvInputStreamFilterSame(array $expected, string $csv_text, int $stream_chunk_size, array $stream_wrapper) : void
     {
-        $steram_wrapper['resource']   = 'php://temp';
+        $stream_wrapper['resource']   = 'php://temp';
 
-        $write_stream   = $this->convertSteramWrapper($steram_wrapper);
+        $write_stream   = $this->convertStreamWrapper($stream_wrapper);
 
         $fp     = @\fopen($write_stream, 'ab');
 
@@ -123,13 +123,13 @@ trait StreamFilterTestTrait
      * @param   string  $expected           予想される値
      * @param   array   $csv_data           実行する値
      * @param   int     $stream_chunk_size  ストリームラッパーのチャンクサイズ
-     * @param   array   $steram_wrapper     ストリームコンテキスト
+     * @param   array   $stream_wrapper     ストリームコンテキスト
      */
-    protected function assertCsvOutputStreamFilterSame(string $expected, array $csv_data, int $stream_chunk_size, array $steram_wrapper) : void
+    protected function assertCsvOutputStreamFilterSame(string $expected, array $csv_data, int $stream_chunk_size, array $stream_wrapper) : void
     {
-        $steram_wrapper['resource']   = 'php://temp';
+        $stream_wrapper['resource']   = 'php://temp';
 
-        $write_stream   = $this->convertSteramWrapper($steram_wrapper);
+        $write_stream   = $this->convertStreamWrapper($stream_wrapper);
 
         $fp     = @\fopen($write_stream, 'ab');
 
@@ -160,16 +160,50 @@ trait StreamFilterTestTrait
     /**
      * Stream Wrapper設定を文字列表現に変換します。
      *
-     * @param   array   $steram_wrapper ストリームラッパー設定
+     * @param   array   $stream_wrapper ストリームラッパー設定
      * @return  string  ストリームラッパー設定
      */
-    protected function convertSteramWrapper(array $steram_wrapper) : string
+    protected function convertStreamWrapper(array $stream_wrapper) : string
     {
         $stack  = [];
-        foreach ($steram_wrapper as $key => $context) {
+        foreach ($stream_wrapper as $key => $context) {
             $stack[]    = \sprintf('%s=%s', $key, \implode('|', (array) $context));
         }
 
         return \sprintf('php://filter/%s', \implode('/', $stack));
+    }
+
+    /**
+     * 整数値で表現されたコードポイントをUTF-8文字に変換する。
+     *
+     * @param   int     $code_point UTF-8文字に変換したいコードポイント
+     * @return  string  コードポイントから作成したUTF-8文字
+     */
+    protected function int2utf8($code_point) {
+        //UTF-16コードポイント内判定
+        if ($code_point < 0) {
+            throw new \Exception(sprintf('%1$s is out of range UTF-16 code point (0x000000 - 0x10FFFF)', $code_point));
+        }
+        if (0x10FFFF < $code_point) {
+            throw new \Exception(sprintf('0x%1$X is out of range UTF-16 code point (0x000000 - 0x10FFFF)', $code_point));
+        }
+
+        //サロゲートペア判定
+        if (0xD800 <= $code_point && $code_point <= 0xDFFF) {
+            throw new \Exception(sprintf('0x%X is in of range surrogate pair code point (0xD800 - 0xDFFF)', $code_point));
+        }
+
+        //1番目のバイトのみでchr関数が使えるケース
+        if ($code_point < 0x80) {
+            return \chr($code_point);
+        }
+
+        //2番目のバイトを考慮する必要があるケース
+        if ($code_point < 0xA0) {
+            return \chr(0xC0 | $code_point >> 6) . \chr(0x80 | $code_point & 0x3F);
+        }
+
+        //数値実体参照表記からの変換
+        return \html_entity_decode('&#'. $code_point .';');
     }
 }
