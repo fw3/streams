a4522d5351a8697b19ae71a8aa9b348d4973ce6a
ICKX <effy.staffs@gmail.com>
2020-08-28 04:43:08 +0900

    release merge (#17)
    * #7 読み込みモード時にConvertEncodingFilterとConvertLienFeedFilterを併用しCSV入力を行うと失敗する (#8) (#9)
    * #13 より容易にフィルタを扱えるようにする。

--- tests/filters/utilitys/StreamFilterSpecTest.php
+++ tests/filters/utilitys/StreamFilterSpecTest.php
@@ -21,21 +21,18 @@ declare(strict_types=1);
 namespace Tests\streams\filters\utilitys;
 
 use PHPUnit\Framework\TestCase;
+use fw3\streams\filters\ConvertEncodingFilter;
 use fw3\streams\filters\utilitys\StreamFilterSpec;
 use fw3\streams\filters\utilitys\specs\StreamFilterConvertEncodingSpec;
 use fw3\streams\filters\utilitys\specs\StreamFilterConvertLinefeedSpec;
+use fw3\tests\streams\traits\StreamFilterTestTrait;
 
 /**
  * ストリームフィルタスペックのテスト
  */
 class StreamFilterSpecTest extends TestCase
 {
-    /**
-     * Setup
-     */
-    protected function setUp() : void
-    {
-    }
+    use StreamFilterTestTrait;
 
     /**
      * Specインスタンスのテスト
@@ -55,5 +52,258 @@ class StreamFilterSpecTest extends TestCase
 
         $this->assertSame($streamFilterSpec->with()->appendWriteChain($streamFilterConvertLinefeedSpec->with()->setupForUnix())->build(), 'php://filter/write=convert.linefeed.LF:ALL');
         $this->assertSame($streamFilterSpec->with()->appendWriteChain($streamFilterConvertLinefeedSpec->with()->setupForWindows())->build(), 'php://filter/write=convert.linefeed.CRLF:ALL');
+    }
+
+    /**
+     * 指定された名前のストリームフィルタが登録されているかのテスト
+     *
+     * @runInSeparateProcess
+     */
+    public function testExistStreamFilterName(): void
+    {
+        $this->assertFalse(StreamFilterSpec::registeredStreamFilterName(StreamFilterConvertEncodingSpec::registerFilterName()));
+        $this->assertFalse(StreamFilterSpec::registeredStreamFilterName(StreamFilterConvertLinefeedSpec::registerFilterName()));
+
+        $this->assertFalse(StreamFilterSpec::registeredStreamFilterName(StreamFilterConvertEncodingSpec::DEFAULT_FILTER_NAME));
+        $this->assertFalse(StreamFilterSpec::registeredStreamFilterName(StreamFilterConvertLinefeedSpec::DEFAULT_FILTER_NAME));
+
+        StreamFilterSpec::registerConvertEncodingFilter();
+        StreamFilterSpec::registerConvertLinefeedFilter();
+
+        $this->assertTrue(StreamFilterSpec::registeredStreamFilterName(StreamFilterConvertEncodingSpec::registerFilterName()));
+        $this->assertTrue(StreamFilterSpec::registeredStreamFilterName(StreamFilterConvertLinefeedSpec::registerFilterName()));
+
+        $this->assertFalse(StreamFilterSpec::registeredStreamFilterName(StreamFilterConvertEncodingSpec::DEFAULT_FILTER_NAME));
+        $this->assertFalse(StreamFilterSpec::registeredStreamFilterName(StreamFilterConvertLinefeedSpec::DEFAULT_FILTER_NAME));
+    }
+
+    /**
+     * CSV入出力を行うにあたって必要な事前・事後処理を行い、$callbackで指定された処理のテスト
+     */
+    public function testDecorateForCsv(): void
+    {
+        //----------------------------------------------
+        $system_locale                  = ConvertEncodingFilter::startChangeLocale();
+        $system_substitute_character    = ConvertEncodingFilter::startChangeSubstituteCharacter();
+
+        $expected   = [
+            ['ソソソソん', 'ソ ソ ソ ソ ソ ', 'ソソソソん①㈱㌔髙﨑纊ソｱｲｳｴｵあいうえおabc'],
+            ['ソソソソん', 'ソ ソ ソ ソ ソ ', 'ソソソソん①㈱㌔髙﨑纊ソｱｲｳｴｵあいうえおabc'],
+            ['ソソソソん', 'ソ ソ ソ ソ ソ ', 'ソソソソん①㈱㌔髙﨑纊ソｱｲｳｴｵあいうえおabc'],
+        ];
+        $actual     = StreamFilterSpec::decorateForCsv(function () use ($expected) {
+            $data   = $expected;
+
+            $spec   = StreamFilterSpec::resourceTemp()->write([
+                StreamFilterConvertEncodingSpec::toSjisWin()->fromUtf8(),
+                StreamFilterConvertLinefeedSpec::toCrLf()->fromAll(),
+            ])->read([
+                StreamFilterConvertEncodingSpec::toUtf8()->fromSjisWin(),
+            ]);
+
+            $fp     = \fopen($spec->build(), 'r+b');
+
+            // 書き込み
+            foreach ($data as $datum) {
+                \fputcsv($fp, $datum);
+            }
+
+            // 読み込み
+            \rewind($fp);
+
+            $rows   = [];
+            for (;($row = \fgetcsv($fp, 1024)) !== FALSE;$rows[] = $row);
+
+            \fclose($fp);
+
+            return $rows;
+        });
+
+        $this->assertEquals($expected, $actual);
+
+        ConvertEncodingFilter::endChangeSubstituteCharacter();
+        $this->assertEquals($system_substitute_character, ConvertEncodingFilter::currentSubstituteCharacter());
+
+        ConvertEncodingFilter::endChangeLocale();
+        $this->assertEquals($system_locale, ConvertEncodingFilter::currentLocale());
+
+        //----------------------------------------------
+        $system_locale                  = ConvertEncodingFilter::startChangeLocale();
+        $system_substitute_character    = ConvertEncodingFilter::startChangeSubstituteCharacter();
+
+        $data   = [
+            ['𩸽鎽艗神'],
+        ];
+
+        $expected   = [
+            ['U+29E3DU+93BDU+8257神'],
+        ];
+        $actual     = StreamFilterSpec::decorateForCsv(function () use ($data) {
+            $spec   = StreamFilterSpec::resourceTemp()->write([
+                StreamFilterConvertEncodingSpec::toSjisWin()->fromUtf8(),
+                StreamFilterConvertLinefeedSpec::toCrLf()->fromAll(),
+            ])->read([
+                StreamFilterConvertEncodingSpec::toUtf8()->fromSjisWin(),
+            ]);
+
+            $fp     = \fopen($spec->build(), 'r+b');
+
+            // 書き込み
+            foreach ($data as $datum) {
+                \fputcsv($fp, $datum);
+            }
+
+            // 読み込み
+            \rewind($fp);
+
+            $rows   = [];
+            for (;($row = \fgetcsv($fp, 1024)) !== FALSE;$rows[] = $row);
+
+            \fclose($fp);
+
+            return $rows;
+        }, ConvertEncodingFilter::SUBSTITUTE_CHARACTER_LONG);
+
+        $this->assertEquals($expected, $actual);
+
+        ConvertEncodingFilter::endChangeSubstituteCharacter();
+        $this->assertEquals($system_substitute_character, ConvertEncodingFilter::currentSubstituteCharacter());
+
+        ConvertEncodingFilter::endChangeLocale();
+        $this->assertEquals($system_locale, ConvertEncodingFilter::currentLocale());
+
+        //----------------------------------------------
+        $system_locale                  = ConvertEncodingFilter::startChangeLocale();
+        $system_substitute_character    = ConvertEncodingFilter::startChangeSubstituteCharacter();
+
+        $data   = [
+            ['あかさたな'],
+        ];
+
+        $expected   = [
+            ['あかさたな'],
+        ];
+        $actual     = StreamFilterSpec::decorateForCsv(function () use ($data) {
+            $spec   = StreamFilterSpec::resourceTemp()->write([
+                StreamFilterConvertEncodingSpec::toSjisWin(),
+                StreamFilterConvertLinefeedSpec::toCrLf()->fromAll(),
+            ])->read([
+                StreamFilterConvertEncodingSpec::toUtf8(),
+            ]);
+
+            $fp     = \fopen($spec->build(), 'r+b');
+
+            // 書き込み
+            foreach ($data as $datum) {
+                \fputcsv($fp, $datum);
+            }
+
+            // 読み込み
+            \rewind($fp);
+
+            $rows   = [];
+            for (;($row = \fgetcsv($fp, 1024)) !== FALSE;$rows[] = $row);
+
+            \fclose($fp);
+
+            return $rows;
+        }, null, ['UTF-8', 'SJIS-win']);
+
+        $this->assertEquals($expected, $actual);
+
+        ConvertEncodingFilter::endChangeSubstituteCharacter();
+        $this->assertEquals($system_substitute_character, ConvertEncodingFilter::currentSubstituteCharacter());
+
+        ConvertEncodingFilter::endChangeLocale();
+        $this->assertEquals($system_locale, ConvertEncodingFilter::currentLocale());
+
+        //----------------------------------------------
+        $system_locale                  = ConvertEncodingFilter::startChangeLocale();
+        $system_substitute_character    = ConvertEncodingFilter::startChangeSubstituteCharacter();
+
+        $data   = [
+            ['あかさたな'],
+        ];
+
+        $expected   = [
+            ['あかさたな'],
+        ];
+        $actual     = StreamFilterSpec::decorateForCsv(function () use ($data) {
+            $spec   = StreamFilterSpec::resourceTemp()->write([
+                StreamFilterConvertEncodingSpec::toSjisWin()->fromUtf8(),
+                StreamFilterConvertLinefeedSpec::toCrLf()->fromAll(),
+            ])->read([
+                StreamFilterConvertEncodingSpec::toUtf8()->fromSjisWin(),
+            ]);
+
+            $fp     = \fopen($spec->build(), 'r+b');
+
+            // 書き込み
+            foreach ($data as $datum) {
+                \fputcsv($fp, $datum);
+            }
+
+            // 読み込み
+            \rewind($fp);
+
+            $rows   = [];
+            for (;($row = \fgetcsv($fp, 1024)) !== FALSE;$rows[] = $row);
+
+            \fclose($fp);
+
+            return $rows;
+        }, null, null, ConvertEncodingFilter::getSafeLocale());
+
+        $this->assertEquals($expected, $actual);
+
+        ConvertEncodingFilter::endChangeSubstituteCharacter();
+        $this->assertEquals($system_substitute_character, ConvertEncodingFilter::currentSubstituteCharacter());
+
+        ConvertEncodingFilter::endChangeLocale();
+        $this->assertEquals($system_locale, ConvertEncodingFilter::currentLocale());
+
+        //----------------------------------------------
+        $expected   = [
+        ];
+
+        try {
+            $actual     = StreamFilterSpec::decorateForCsv(function () use ($expected) {
+                $data   = $expected;
+
+                if (empty($data)) {
+                    throw new \Exception('空の配列を指定されました。');
+                }
+
+                $spec   = StreamFilterSpec::resourceTemp()->write([
+                    StreamFilterConvertEncodingSpec::toSjisWin()->fromUtf8(),
+                    StreamFilterConvertLinefeedSpec::toCrLf()->fromAll(),
+                ])->read([
+                    StreamFilterConvertEncodingSpec::toUtf8()->fromSjisWin(),
+                ]);
+
+                $fp     = \fopen($spec->build(), 'r+b');
+
+                // 書き込み
+                foreach ($data as $datum) {
+                    \fputcsv($fp, $datum);
+                }
+
+                // 読み込み
+                \rewind($fp);
+
+                $rows   = [];
+                for (;($row = \fgetcsv($fp, 1024)) !== FALSE;$rows[] = $row);
+
+                \fclose($fp);
+
+                return $rows;
+            });
+        } catch (\Exception $e) {
+            $this->assertEquals('空の配列を指定されました。', $e->getMessage());
+
+            $this->assertEquals($system_substitute_character, ConvertEncodingFilter::currentSubstituteCharacter());
+
+            $this->assertEquals($system_locale, ConvertEncodingFilter::currentLocale());
+        }
     }
 }
